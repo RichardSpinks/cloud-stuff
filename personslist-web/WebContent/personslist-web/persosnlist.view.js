5c2ea164ab6aa2ffb7546a4871a08e995eb42433
RichardSpinks <richard.spinks@presenceofit.com.au>
2013-09-05 14:33:53 +1000

    New personslist

---
+++ personslist-web/WebContent/personslist-web/persosnlist.view.js
@@ -0,0 +1,93 @@
+sap.ui.jsview("personslist-web.persosnlist", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf personslist-web.persosnlist
+	*/ 
+	getControllerName : function() {
+		return "personslist-web.persosnlist";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf personslist-web.persosnlist
+	*/ 
+	createContent : function(oController) {
+
+//		// create the button instance
+//		var myButton = new sap.ui.commons.Button("btn"); 
+//		// set properties, e.g. the text (there is also a shorter way of setting several properties) 
+//		myButton.setText("Hello World!"); 
+//		// attach an action to the button's "press" event (use jQuery to fade out the button) 
+//		myButton.attachPress(function(){$("#btn").fadeOut();});
+//		return myButton;
+		
+		// Create an instance of the table control 
+		var oTable = new sap.ui.table.Table({ 
+					title : "Persons List", 
+					visibleRowCount : 7, 
+					firstVisibleRow : 3, 
+					selectionMode : sap.ui.table.SelectionMode.Single, 
+			}); 
+		
+		// toolbar 
+		var oTableToolbar = new sap.ui.commons.Toolbar(); 
+		
+		// first name field 
+		var oFirstNameLabel = new sap.ui.commons.Label({ 
+					text : 'First Name' 
+			}); 
+		var oFirstNameField = new sap.ui.commons.TextField({ 
+					id : 'firstNameFieldId', 
+					value : '', 
+					width : '10em', 
+			}); 
+		oFirstNameLabel.setLabelFor(oFirstNameField); 
+		oTableToolbar.addItem(oFirstNameLabel); 
+		oTableToolbar.addItem(oFirstNameField); 
+		
+		// last name field 
+		var oLastNameLabel = new sap.ui.commons.Label({ 
+				text : 'Last Name' 
+			}); 
+		var oLastNameField = new sap.ui.commons.TextField({ 
+				id : 'lastNameFieldId', 
+				value : '', 
+				width : '10em', 
+			}); 
+		oLastNameLabel.setLabelFor(oLastNameField); 
+		oTableToolbar.addItem(oLastNameLabel); 
+		oTableToolbar.addItem(oLastNameField); 
+		
+		// add button 
+		var oAddPersonButton = new sap.ui.commons.Button({ 
+				id : 'addPersonButtonId', 
+				text : "Add Person", 
+				press : function() { 
+					oController.addNewPerson(sap.ui.getCore().getControl( 
+					"firstNameFieldId").getValue(), sap.ui.getCore().getControl("lastNameFieldId").getValue(), oTable); 
+					} 
+			}); 
+		oTableToolbar.addItem(oAddPersonButton); 
+		oTable.setToolbar(oTableToolbar); 
+		
+		// define the columns and the control templates to be used 
+		oTable.addColumn(new sap.ui.table.Column({ 
+				label : new sap.ui.commons.Label({text : "First Name"}), 
+				template : new	sap.ui.commons.TextField().bindProperty("value", "FirstName"), 
+				sortProperty : "FirstName", 
+				filterProperty : "FirstName", 
+				width : "100px"
+			})); 
+		oTable.addColumn(new sap.ui.table.Column({ 
+				label : new sap.ui.commons.Label({text : "Last Name"}), 
+				template : new	sap.ui.commons.TextField().bindProperty("value", "LastName"), 
+				sortProperty : "LastName", 
+				filterProperty : "LastName", 
+				width : "200px"
+			})); 
+		// bind table rows to /Persons based on the model defined in the init method of the controller 
+		oTable.bindRows("/Persons"); 
+		return oTable; 
+		 } 		
+});
